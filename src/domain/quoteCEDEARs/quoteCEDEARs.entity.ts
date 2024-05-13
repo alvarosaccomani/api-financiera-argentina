6456ae500abbb03029f256f593c6b10da401bc33
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2024-05-13 16:33:00 -0300

    add quoteCEDEARs entity to project

---
+++ src/domain/quoteCEDEARs/quoteCEDEARs.entity.ts
@@ -0,0 +1,18 @@
+export interface QuoteCEDEAREntity {
+    ticker: string,
+    timeQuote: string,
+    nominalQty: string,
+    buys: string,
+    sale: string,
+    nominalQty1: string,
+    last: string,
+    variation: string,
+    opening: string,
+    max: string,
+    min: string,
+    closingPrevious: string,
+    volume: string,
+    amount: string,
+    operations: string,
+    hour: string
+}
